12e006e4c810c58e59ba3b29a31d615e67adfeaa
cochranepaul <pmcochrane@gmail.com>
2015-10-02 16:08:30 -0700

    adds routes

---
+++ app/routes.js
@@ -0,0 +1,36 @@
+module.exports = function(app, passport) {
+
+	// 	HOME PAGE log in
+	//	================
+	app.get('/', function(req, res){
+		res.render('index.ejs'); //loads the index.ejs file
+	});
+
+	//	LOGIN
+	//	=====
+	app.get('login', function(req, res){
+		res.render('login.ejs', { message: ('loginMessage')};
+	});
+
+	//app.post('/login' +++++ all the passport stuff goes here)
+
+	//	SIGN UP
+	//	=======
+
+	app.get('/signup', function(req, res) {
+		res.render('signup.ejs', { message: (signupMessage)};
+	});
+
+	//	process the signup form
+	//	app.post('./signup', +++++ all the passport stuff here)
+
+	//	PROFILE
+	//	=======
+
+	app.get('/profile', isLoggedIn, function(req, res){
+		res.render('profile.ejs', {
+			user : req.user // will get the user out of the session
+		})
+	})
+
+}
