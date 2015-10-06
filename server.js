e7166532bf223011d72d700db74c8daeb1317788
Dresosh <contact@dresosh.com>
2015-10-06 01:37:18 -0700

    adds twitter feed functionality

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // server.js set up which incldues the required files
 
-var express 		= require(  "express" ),
+var express 		= require( "express" ),
 	app				= express(),
+	http			= require( 'http' ).Server( app ),
+	io				= require( 'socket.io' )( http ),
 	expressHelper	= require( 'express-helpers' )
 	port 			= process.env.PORT || 8080,
 	mongoose		= require( 'mongoose' ),
@@ -16,7 +18,8 @@ var express 		= require(  "express" ),
 	vendorRouter	= require( './app/routes/vendorRoutes' ),
 	userRouter		= require( './app/routes/userRoutes' ),
 	helpers			= require( 'express-helpers' ),
-	path 			= require( 'path' )
+	path 			= require( 'path' ),
+	Twit			= require( 'twit' )
 
 
 
@@ -24,13 +27,13 @@ var express 		= require(  "express" ),
 //	CONFIG
 //	======
 
-mongoose.connect(   configDB.url ) // connects to the database
+mongoose.connect( configDB.url ) // connects to the database
 
-require(  "./config/passport"  )( passport ) //passes in passport for configuration
+require( "./config/passport" )( passport ) //passes in passport for configuration
 
 require( 'express-helpers' )( app );
 // set up express application
-app.use( morgan(  "dev" ) )
+app.use( morgan( "dev" ) )
 app.use( cookieParser() ) //reads cookies which are needed for authentication
 app.use( bodyParser.urlencoded( { extended: true} ) ) // gets info from the html form
 app.use( bodyParser.json() )
@@ -38,11 +41,11 @@ app.use( flash() )
 helpers( app )
 app.use( express.static( path.join( __dirname, 'public' )))
 
-app.set(  "view engine", "ejs" ) //sets up ejs for templating
-app.engine(  "ejs", require(  "ejs" ).renderFile )
+app.set( "view engine", "ejs" ) //sets up ejs for templating
+app.engine( "ejs", require( "ejs" ).renderFile )
 
 // passport requirements
-app.use(  session( {
+app.use( session( {
 	secret: "supersupersecret",
 	resave: true,
 	saveUninitialized: true
@@ -64,7 +67,50 @@ require( './app/routes/vendorRoutes.js' );
 app.use( '/deals', dealRouter ); //when you get a request starting with deal use dealRouter
 app.use( '/vendors', vendorRouter )
 
+
+
+
+// TWITTER FEED
+
+var twitter = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var stream;
+var searchTerm;
+
+io.on('connect', function(socket) {
+  socket.on('updateTerm', function (searchTerm) {
+    socket.emit('updatedTerm', searchTerm);
+
+    // Start stream
+    if (stream) {
+      stream.stop();
+    }
+
+    stream = twitter.stream('statuses/filter', { track: searchTerm, language: 'en' });
+
+    stream.on('tweet', function (tweet) {
+      var data = {};
+      data.name = tweet.user.name;
+      data.screen_name = tweet.user.screen_name;
+      data.text = tweet.text;
+      data.user_profile_image = tweet.user.profile_image_url;
+      socket.emit('tweets', data);
+    });
+  });
+});
+
+
+
+
+
+
+
 //	LAUNCH
 //	======
-app.listen(  port )
-console.log(  "The magic is happening on port " + port )
+app.listen( port )
+console.log( "The magic is happening on port " + port )
