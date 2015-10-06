da45d49940c6b400f863427a29522b166ddd75ff
Dresosh <contact@dresosh.com>
2015-10-06 02:29:07 -0700

    add spacing

--- app/controllers/vendorsController.js
+++ app/controllers/vendorsController.js
@@ -1,66 +1,66 @@
-var Vendor = require('../models/vendor.js')
+var Vendor = require( '../models/vendor.js' )
 
-function index (req, res){
+function index ( req, res ) {
 		//gets all the users
-		Vendor.find(function(err, users){
-			if(err) res.send (err)
-			res.json(users)
+		Vendor.find( function( err, users ) {
+			if( err ) res.send ( err )
+			res.json( users )
 		})
 	}
 
-function create (req, res){
+function create ( req, res ) {
 		//makes single user
 		var vendor = new Vendor()
 		vendor.name = req.body.name
 		vendor.email = req.body.email
 		vendor.password = req.body.password
 
-		vendor.save(function(err){
-			if(err){
-				if(err.code == 11000){
-					res.json({success: false, message: "email already exists"})
+		vendor.save( function( err ) {
+			if( err ) {
+				if( err.code == 11000 ) {
+					res.json( {success: false, message: "email already exists"})
 				} else {
-					res.send(err)
+					res.send( err )
 				}
 			}
-			res.json({success: true, message: "vendor created!"})
+			res.json( {success: true, message: "vendor created!"})
 	})
 
 }
 
-function show (req, res){
+function show ( req, res ) {
 		//gets a single vendor
-		Vendor.findById(req.params.vendor_id, function(err, vendor){
-			if(err) res.send(err)
-			res.json(vendor)
+		Vendor.findById( req.params.vendor_id, function( err, vendor ) {
+			if( err ) res.send( err )
+			res.json( vendor )
 		})
 
 	}
 
-function update (req, res){
+function update ( req, res ) {
 		//update a vendor
-	vendor.findById(req.params.vendor_id, function(err, vendor){
-		if(err) res.send
+	vendor.findById( req.params.vendor_id, function( err, vendor ) {
+		if( err ) res.send
 
-		if(req.body.name) vendor.name = req.body.name
-		if(req.body.email) vendor.email = req.body.email
-		if(req.body.password) vendor.password = req.body.password
+		if( req.body.name ) vendor.name = req.body.name
+		if( req.body.email ) vendor.email = req.body.email
+		if( req.body.password ) vendor.password = req.body.password
 
-		vendor.save(function(err){
-			if(err) res.send(err)
-			res.json({success: true, message: "You have been updated!"})
+		vendor.save( function( err ) {
+			if( err ) res.send( err )
+			res.json( {success: true, message: "You have been updated!"})
 		})
-		
+
 	})
 }
 
-function destroy (req, res){
+function destroy ( req, res ) {
 		//deletes a vendor
-	Vendor.remove({
+	Vendor.remove( {
 		_id: req.params.vendor_id
-	}, function(err, user){
-		if(err) res.send(err)
-		res.json({success: true, message: "You have been terminated!"})
+	}, function( err, user ) {
+		if( err ) res.send( err )
+		res.json( {success: true, message: "You have been terminated!"})
 	})
 }
 
@@ -71,4 +71,3 @@ module.exports = {
 	update: update,
 	destroy: destroy
 }
-
--- app/models/deal.js
+++ app/models/deal.js
@@ -1,10 +1,10 @@
-var mongoose 	= require('mongoose'),
+var mongoose 	= require( 'mongoose' ),
 	Schema		= mongoose.Schema,
 
 // makes the deal schema
 
-	DealSchema = new Schema({
-		title:  {type: String, required: true}, 
+	DealSchema = new Schema( {
+		title:  {type: String, required: true},
 		price:  {type: Number, required: true},
 		// date:  	{type: Date, required: true},
 		// vendor: [vendorSchema],
@@ -12,10 +12,10 @@ var mongoose 	= require('mongoose'),
 	})
 
 
-// saves the created at date before creation 
-DealSchema.pre('save', function (next) {
+// saves the created at date before creation
+DealSchema.pre( 'save', function ( next ) {
 	this.created_at = new Date();
 	next();
 });
 
-module.exports = mongoose.model('Deal', DealSchema)
+module.exports = mongoose.model( 'Deal', DealSchema )
